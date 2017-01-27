0e01ddba0c9a953e51c8bc2c71aaa082b996197c
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-01-27 15:11:47 +0000

    Placed DFE_EEPROM_ADDR in Host.

--- scs_host/sys/host.py
+++ scs_host/sys/host.py
@@ -15,6 +15,8 @@ class Host(object):
     I2C_EEPROM =        2
     I2C_SENSORS =       2
 
+    DFE_EEPROM_ADDR =   0x54
+
     SCS_CONF = "/home/debian/SCS/conf/"         # hard-coded path
     SCS_OSIO = "/home/debian/SCS/osio/"         # hard-coded path
 
@@ -22,7 +24,7 @@ class Host(object):
     # ----------------------------------------------------------------------------------------------------------------
 
     @staticmethod
-    def enable_eeprom_write():
+    def enable_eeprom_write():          # TODO: rename enable_eeprom_access
         # nothing needs to be done
         pass
 
