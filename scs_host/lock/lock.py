7e2070b2bda05719385b2de441804bb5595a78bf
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-03-16 11:38:48 +0000

    Removed chmod from locks.

--- scs_host/lock/lock.py
+++ scs_host/lock/lock.py
@@ -140,10 +140,10 @@ class Lock(object):
 
         try:
             os.mkdir(name_dir)
-            os.chmod(name_dir, 0o777)
+            # os.chmod(name_dir, 0o777)
 
             os.mkdir(ident_dir)
-            os.chmod(ident_dir, 0o777)
+            # os.chmod(ident_dir, 0o777)
 
             return True
 
