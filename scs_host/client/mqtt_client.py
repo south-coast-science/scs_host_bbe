7b3bd762dbdf12b616e68f6744f08ce1332ac0e3
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-03-19 06:37:26 +0000

    Set MQTTClient QoS to 1.

--- scs_host/client/mqtt_client.py
+++ scs_host/client/mqtt_client.py
@@ -4,9 +4,14 @@ Created on 11 Nov 2016
 @author: Bruno Beloff (bruno.beloff@southcoastscience.com)
 
 https://pypi.python.org/pypi/paho-mqtt
+http://www.hivemq.com/blog/mqtt-client-library-paho-python
 
-mosquitto_pub -h mqtt.opensensors.io -i <DeviceID> -t /users/<UserName>/<TopicName> -m 'This is a test message' -u <UserName> -P <Device Password>
-mosquitto_pub -h mqtt.opensensors.io -i 5402 -t /users/southcoastscience-dev/test/text -m 'hello' -u southcoastscience-dev -P cPhbitmp
+
+mosquitto_pub -h mqtt.opensensors.io -i <DeviceID> -t /users/<UserName>/<TopicName> -m 'This is a test message' \
+-u <UserName> -P <Device Password>
+
+mosquitto_pub -h mqtt.opensensors.io -i 5402 -t /users/southcoastscience-dev/test/text -m 'hello' \
+-u southcoastscience-dev -P cPhbitmp
 """
 
 import paho.mqtt.publish as publish
