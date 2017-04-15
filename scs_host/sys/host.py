6cdd3a4e472f44839deb2d0b109c4c7b9de43cb3
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-04-15 16:33:24 +0100

    Added serial_number() method to Host.

--- scs_host/sys/host.py
+++ scs_host/sys/host.py
@@ -2,8 +2,12 @@
 Created on 16 Nov 2016
 
 @author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+
+http://dumb-looks-free.blogspot.co.uk/2014/05/beaglebone-black-bbb-revision-serial.html
+
 """
 
+import os
 import subprocess
 
 
@@ -30,6 +34,13 @@ class Host(object):
 
 
     # ----------------------------------------------------------------------------------------------------------------
+
+    @staticmethod
+    def serial_number():
+        serial = os.popen("hexdump -e '8/1 \"%c\"' /sys/bus/i2c/devices/0-0050/eeprom -s 16 -n 12").readline()
+
+        return serial
+
 
     @staticmethod
     def power_cycle():
