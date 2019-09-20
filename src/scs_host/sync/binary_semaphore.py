9ee0cd61f477a171336454de9219e840f075d79b
Bruno Beloff <bruno.beloff@southcoastscience.com>
2019-09-20 09:39:03 +0100

    Fixed a race condition in BinarySemaphore.

--- src/scs_host/sync/binary_semaphore.py
+++ src/scs_host/sync/binary_semaphore.py
@@ -19,6 +19,8 @@ class BinarySemaphore(object):
     classdocs
     """
 
+    __INITIAL_ACQUISITION_TIME = 1.0                    # seconds
+
     # ----------------------------------------------------------------------------------------------------------------
 
     def __init__(self, name):
@@ -28,14 +30,19 @@ class BinarySemaphore(object):
         self.__name = name
         self.__semaphore = posix_ipc.Semaphore(self.name, flags=posix_ipc.O_CREAT)
 
+        try:
+            self.__semaphore.acquire(self.__INITIAL_ACQUISITION_TIME)       # initial state is: acquired by creator
+        except (posix_ipc.BusyError, posix_ipc.SignalError):
+            pass
+
 
     # ----------------------------------------------------------------------------------------------------------------
 
-    def acquire(self, timeout=None):
+    def acquire(self, timeout=None):                    # None timeout = wait forever (dangerous)
         self.__semaphore.acquire(timeout)
 
         while self.__semaphore.value > 0:
-            self.__semaphore.acquire()                    # limit the value to 0 or 1
+            self.__semaphore.acquire()                  # limit the value to 0 or 1
 
 
     def release(self):
